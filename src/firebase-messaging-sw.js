a1e40a77e21f0b0c91c203c91aad971c2ebdc59e
Vikas Dhiman <62803759+vikasdhiman0635@users.noreply.github.com>
2020-11-20 23:18:45 +0530

    Update firebase-messaging-sw.js

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,14 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCybLsa48bxFYHqNHVosvz_M52H8Pe635c",
-    authDomain: "chatwork-81276.firebaseapp.com",
-    databaseURL: "https://chatwork-81276.firebaseio.com",
-    projectId: "chatwork-81276",
-    storageBucket: "chatwork-81276.appspot.com",
-    messagingSenderId: "65535727055",
-    appId: "1:65535727055:web:46c93ff27eacc9ebd0eeba",
-    measurementId: "G-CF63Q8GWYD"
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXXXXXXX"
 });
 
 const messaging = firebase.messaging();
