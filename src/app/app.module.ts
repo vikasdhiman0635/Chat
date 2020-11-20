f62f4c7599f34d87992b17508900e6b4593566b7
Vikas Dhiman <62803759+vikasdhiman0635@users.noreply.github.com>
2020-11-20 23:21:19 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,14 @@ import { AsyncPipe } from '@angular/common';
 import { MessagingService } from './Service/messaging.service';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCybLsa48bxFYHqNHVosvz_M52H8Pe635c",
-    authDomain: "chatwork-81276.firebaseapp.com",
-    databaseURL: "https://chatwork-81276.firebaseio.com",
-    projectId: "chatwork-81276",
-    storageBucket: "chatwork-81276.appspot.com",
-    messagingSenderId: "65535727055",
-    appId: "1:65535727055:web:46c93ff27eacc9ebd0eeba",
-    measurementId: "G-CF63Q8GWYD"
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXX"
 };
 @NgModule({
   declarations: [
