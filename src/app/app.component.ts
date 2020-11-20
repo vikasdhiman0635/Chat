d0ecccead62db2b9bc5d0d411a65d583c841b2bd
Vikas Dhiman <62803759+vikasdhiman0635@users.noreply.github.com>
2020-11-20 23:12:27 +0530

    firstbranch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessagingService } from "../app/Service/messaging.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Chat';
+  message;
+  constructor(private messagingService: MessagingService) { }
+  ngOnInit() {
+    this.messagingService.requestPermission()
+    this.messagingService.receiveMessage()
+    this.message = this.messagingService.currentMessage
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireMessagingModule } from "@angular/fire/messaging";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireModule } from '@angular/fire';
+import { AsyncPipe } from '@angular/common';
+import { MessagingService } from './Service/messaging.service';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCybLsa48bxFYHqNHVosvz_M52H8Pe635c",
+    authDomain: "chatwork-81276.firebaseapp.com",
+    databaseURL: "https://chatwork-81276.firebaseio.com",
+    projectId: "chatwork-81276",
+    storageBucket: "chatwork-81276.appspot.com",
+    messagingSenderId: "65535727055",
+    appId: "1:65535727055:web:46c93ff27eacc9ebd0eeba",
+    measurementId: "G-CF63Q8GWYD"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireMessagingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [MessagingService,AsyncPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
